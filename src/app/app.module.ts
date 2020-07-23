ad2de5a2a7827fe3076fb82b922b8950c564be0c
Elton Dornelas <eltondornelas@gmail.com>
2020-07-23 16:13:14 -0300

    Adicionando model, service, list e form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from '../service/task.service';
+import { Task } from '../model/task';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent implements OnInit {
+
+  task: Task;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private taskService: TaskService) {
+
+    this.task = new Task();
+  }
+
+
+  onSubmit() {
+    this.taskService.save(this.task).subscribe(result => this.gotoTaskList());
+  }
+
+
+  gotoTaskList() {
+    this.router.navigate(['/tarefas']);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
